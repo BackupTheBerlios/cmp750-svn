daae225236b5f7a802edcb9af69a8aaffe8b9965
ert <ert@1f02285e-f004-0410-be6c-aa8e4cb6c028>
2005-12-25 21:02:01 +0000

    Implemented init

--- trunk/cmp750.c
+++ trunk/cmp750.c
@@ -1,11 +1,9 @@
-/* sane - Scanner Access Now Easy.
+/* cmp750.c - SANE backend for the CANON PIXMA MP750 Backend.
 
-   CANON PIXMA MP750 Backend
    Copyright (C) 2005, 2006 Christopher Albert
    ert@zapo.net - http://see.tang.la
 
-   written using the Lexmark Backend
-   as a template
+   written using the Lexmark Backend as a template
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -21,27 +19,6 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330, Boston,
    MA 02111-1307, USA.
-
-   As a special exception, the authors of SANE give permission for
-   additional uses of the libraries contained in this release of SANE.
-
-   The exception is that, if you link a SANE library with other files
-   to produce an executable, this does not by itself cause the
-   resulting executable to be covered by the GNU General Public
-   License.  Your use of that executable is in no way restricted on
-   account of linking the SANE library code into it.
-
-   This exception does not, however, invalidate any other reasons why
-   the executable file might be covered by the GNU General Public
-   License.
-
-   If you submit changes to SANE to the maintainers to be included in
-   a subsequent release, you agree by submitting the changes that
-   those changes may be distributed with this exception intact.
-
-   If you write modifications of your own for SANE, it is your choice
-   whether to permit this exception to apply to your modifications.
-   If you do not wish that, delete this exception notice.
 
 */
 
@@ -69,11 +46,114 @@
 #include "cmp750.h"
 #include "../include/sane/sanei_backend.h"
 
+static CMP750_Device *first_cmp750_device = 0;
+static SANE_Int num_cmp750_device = 0;
+static const SANE_Device **sane_device_list = NULL;
+
+/*
+*    WIP
+*/
+
+SANE_Status
+attachCMP750 (SANE_String_Const devname)
+{
+  CMP750_Device *cmp750_device;
+
+  DBG (2, "attachCMP750: devname=%s\n", devname);
+
+  for (cmp750_device = first_cmp750_device; cmp750_device;
+       cmp750_device = cmp750_device->next)
+    {
+      if (strcmp (cmp750_device->sane.name, devname) == 0)
+	    return SANE_STATUS_GOOD;
+    }
+
+  cmp750_device = (CMP750_Device *) malloc (sizeof (CMP750_Device));
+  if (cmp750_device == NULL)
+    return SANE_STATUS_NO_MEM;
+
+  cmp750_device->sane.name = strdup (devname);
+  cmp750_device->sane.vendor = "CANON";
+  cmp750_device->sane.model = "MP750";
+  cmp750_device->sane.type = "multi-function peripheral";
+
+  /* Set the default resolution here */
+  cmp750_device->x_dpi = 75;
+  cmp750_device->y_dpi = 75;
+
+  /* Make the pointer to the read buffer null here */
+  cmp750_device->read_buffer = NULL;
+
+  /* Set the default threshold for lineart mode here */
+  cmp750_device->threshold = 0x80;
+
+  cmp750_device->next = first_cmp750_device;
+  first_cmp750_device = cmp750_device;
+
+  num_cmp750_device++;
+
+  return SANE_STATUS_GOOD;
+}
+
+SANE_Status sane_init (SANE_Int * version_code, SANE_Auth_Callback authorize)
+{
+  FILE *fp;
+  SANE_Char line[PATH_MAX];
+  const char *lp;
+  SANE_Int vendor, product;
+  size_t len;
+  SANE_Status status;
+  SANE_Auth_Callback auth_callback;
+  
+  DBG_INIT ();
+  DBG("Initializing cmp750 backend...");
+
+  sanei_usb_init ();
+
+  fp = sanei_config_open ("cmp750.conf");
+  if (!fp)
+    {
+      return SANE_STATUS_ACCESS_DENIED;
+    }
+
+  while (sanei_config_read (line, PATH_MAX, fp))
+    {
+      /* ignore comments */
+      if (line[0] == '#')
+	      continue;
+      len = strlen (line);
+
+      /* delete newline characters at end */
+      if (line[len - 1] == '\n')
+	      line[--len] = '\0';
+
+      /* lp = (SANE_String) sanei_config_skip_whitespace (line); */
+      lp = sanei_config_skip_whitespace (line);
+      /* skip empty lines */
+      if (*lp == 0)
+	      continue;
+
+      if (sscanf (lp, "usb %i %i", &vendor, &product) == 2)
+	      ;
+      else if (strncmp ("libusb", lp, 6) == 0)
+	      ;
+      else if ((strncmp ("usb", lp, 3) == 0) && isspace (lp[3]))
+	    {
+	      lp += 3;
+	      /* lp = (SANE_String) sanei_config_skip_whitespace (lp); */
+	      lp = sanei_config_skip_whitespace (lp);
+	    }
+  
+  sanei_usb_attach_matching_devices (lp, attachCMP750)
+  
+  return SANE_STATUS_GOOD;
+}
+
+
 /*
  *    TODO
  */
- 
-SANE_Status sane_init (SANE_Int * version_code, SANE_Auth_Callback authorize)
+
 
 void sane_exit (void);
 
--- trunk/cmp750.h
+++ trunk/cmp750.h
@@ -1,1 +1,131 @@
+/* cmp750.h - SANE backend for the CANON PIXMA MP750 Backend.
 
+   Copyright (C) 2005, 2006 Christopher Albert
+   ert@zapo.net - http://see.tang.la
+
+   written using the Lexmark Backend as a template
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+   MA 02111-1307, USA.
+
+*/
+
+#ifndef CMP750_H
+#define CMP750_H
+
+/* Force the backend name for all files using this include */
+#ifdef BACKEND_NAME
+#undef BACKEND_NAME
+#define BACKEND_NAME cmp750
+#endif
+
+#define DEBUG_NOT_STATIC
+#define SANE_NAME_PAPER_SIZE "paper-size"
+#define SANE_TITLE_PAPER_SIZE SANE_I18N("Paper size")
+#define SANE_DESC_PAPER_SIZE \
+SANE_I18N("Selects the size of the area to be scanned.");
+
+typedef enum
+{
+  OPT_NUM_OPTS = 0,
+  OPT_MODE,
+/*   OPT_X_DPI, */
+/*   OPT_Y_DPI, */
+  OPT_RESOLUTION,
+  OPT_PREVIEW,
+  OPT_SCAN_SIZE,
+  OPT_THRESHOLD,
+  /* must come last: */
+  NUM_OPTIONS
+}
+CMP750_Options;
+
+typedef enum
+{
+  RED = 0,
+  GREEN,
+  BLUE
+}
+Scan_Regions;
+
+/** @name Option_Value union
+ * convenience union to access option values given to the backend
+ * @{
+ */
+#ifndef SANE_OPTION
+#define SANE_OPTION 1
+typedef union
+{
+  SANE_Bool b;
+  SANE_Word w;
+  SANE_Word *wa;		/* word array */
+  SANE_String s;
+}
+Option_Value;
+#endif
+
+typedef struct Read_Buffer
+{
+  SANE_Int gray_offset;
+  SANE_Int max_gray_offset;
+  SANE_Int region;
+  SANE_Int red_offset;
+  SANE_Int green_offset;
+  SANE_Int blue_offset;
+  SANE_Int max_red_offset;
+  SANE_Int max_green_offset;
+  SANE_Int max_blue_offset;
+  SANE_Byte *data;
+  SANE_Byte *readptr;
+  SANE_Byte *writeptr;
+  SANE_Byte *max_writeptr;
+  size_t size;
+  size_t linesize;
+  SANE_Bool empty;
+  SANE_Int image_line_no;
+  SANE_Int bit_counter;
+  SANE_Int max_lineart_offset;
+}
+Read_Buffer;
+
+typedef struct CMP750_Device
+{
+  struct CMP750_Device *next;
+  
+  SANE_Device sane;
+  SANE_Option_Descriptor opt[NUM_OPTIONS];
+  Option_Value val[NUM_OPTIONS];
+  SANE_Parameters params;
+  SANE_Int devnum;
+  long data_size;
+  SANE_Int pixel_height;
+  SANE_Int pixel_width;
+  SANE_Bool initialized;
+  SANE_Bool eof;
+  SANE_Int x_dpi;
+  SANE_Int y_dpi;
+  long data_ctr;
+  SANE_Bool device_cancelled;
+  SANE_Int cancel_ctr;
+  SANE_Byte *transfer_buffer;
+  size_t bytes_remaining;
+  size_t bytes_in_buffer;
+  SANE_Byte *read_pointer;
+  Read_Buffer *read_buffer;
+  SANE_Byte threshold;
+} 
+CMP750_Device;
+
+#endif /* CMP750_H */
