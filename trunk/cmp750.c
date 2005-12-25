f3a0ed2b262d1d25ef2aaf9a75c20b721e1f6b48
ert <ert@1f02285e-f004-0410-be6c-aa8e4cb6c028>
2005-12-25 19:49:46 +0000

    New skelleton / test

--- trunk/cmp750.c
+++ trunk/cmp750.c
@@ -1,8 +1,11 @@
-/*******************************************************************************
- * Canon MP750 SANE backend
-   based on the ma1509 backend
+/* sane - Scanner Access Now Easy.
 
-   cmp750.c
+   CANON PIXMA MP750 Backend
+   Copyright (C) 2005, 2006 Christopher Albert
+   ert@zapo.net - http://see.tang.la
+
+   written using the Lexmark Backend
+   as a template
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -19,75 +22,91 @@
    Foundation, Inc., 59 Temple Place - Suite 330, Boston,
    MA 02111-1307, USA.
 
-   ******************************************************************************/
+   As a special exception, the authors of SANE give permission for
+   additional uses of the libraries contained in this release of SANE.
+
+   The exception is that, if you link a SANE library with other files
+   to produce an executable, this does not by itself cause the
+   resulting executable to be covered by the GNU General Public
+   License.  Your use of that executable is in no way restricted on
+   account of linking the SANE library code into it.
+
+   This exception does not, however, invalidate any other reasons why
+   the executable file might be covered by the GNU General Public
+   License.
+
+   If you submit changes to SANE to the maintainers to be included in
+   a subsequent release, you agree by submitting the changes that
+   those changes may be distributed with this exception intact.
+
+   If you write modifications of your own for SANE, it is your choice
+   whether to permit this exception to apply to your modifications.
+   If you do not wish that, delete this exception notice.
+
+*/
 
 #include "../include/sane/config.h"
 
-#include <ctype.h>
+#include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <limits.h>
-#include <sys/time.h>
 #include <sys/types.h>
-#include <errno.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <sys/time.h>
 
 #include "../include/sane/sane.h"
 #include "../include/sane/sanei.h"
 #include "../include/sane/saneopts.h"
+#include "../include/sane/sanei_config.h"
 #include "../include/sane/sanei_usb.h"
 
-#define BACKEND_NAME	cmp750
-#include "../include/sane/sanei_backend.h"
-#include "../include/sane/sanei_config.h"
-
 #include "cmp750.h"
+#include "../include/sane/sanei_backend.h"
 
-#ifndef SANE_I18N
-#define SANE_I18N(text) text
-#endif
+/*
+ *    TODO
+ */
+ 
+SANE_Status sane_init (SANE_Int * version_code, SANE_Auth_Callback authorize)
 
-/* Debug level from sanei_init_debug */
-static SANE_Int debug_level;
+void sane_exit (void);
 
-static SANE_Int num_devices;
-static CMP750_Device *first_dev;
-static CMP750_Scanner *first_handle;
-static const SANE_Device **devlist = 0;
+SANE_Status sane_get_devices (const SANE_Device *** device_list, 
+             SANE_Bool local_only);
+             
+SANE_Status sane_open (SANE_String_Const devicename,
+             SANE_Handle * handle);
+             
+void sane_close (SANE_Handle handle);
 
-static int warmup_time = CMP750_WARMUP_TIME;
+const SANE_Option_Descriptor *
+  sane_get_option_descriptor (SANE_Handle handle, SANE_Int option);
+  
+SANE_Status sane_control_option (SANE_Handle handle, SANE_Int option,
+			 SANE_Action action, void *value,
+			 SANE_Int * info);
+			 
+SANE_Status sane_get_parameters (SANE_Handle handle,
+			 SANE_Parameters * params);
+			 
+SANE_Status sane_start (SANE_Handle handle);
 
-/* Array of newly attached devices */
-static CMP750_Device **new_dev;
+SANE_Status sane_read (SANE_Handle handle, SANE_Byte * data,
+			 SANE_Int max_length, SANE_Int * length);
+			 
+void sane_cancel (SANE_Handle handle);
 
-/* Length of new_dev array */
-static SANE_Int new_dev_len;
+SANE_Status sane_set_io_mode (SANE_Handle handle,
+			 SANE_Bool non_blocking);
+			 
+SANE_Status sane_get_select_fd (SANE_Handle handle,
+			 SANE_Int * fd);
+			 
+SANE_String_Const sane_strstatus (SANE_Status status);
 
-/* Number of entries alloced for new_dev */
-static SANE_Int new_dev_alloced;
-
-static SANE_String_Const mode_list[] = {
-  SANE_I18N ("Lineart"), SANE_I18N ("Gray"), SANE_I18N ("Color"),
-  0
-};
-
-static void
-print_data_buffer (const SANE_Byte * buffer, size_t len)
-{
-  SANE_Byte buffer_byte_list[50];
-  SANE_Byte buffer_byte[5];
-  const SANE_Byte *pp;
-
-  buffer_byte_list[0] = '\0';
-  for (pp = buffer; pp < (buffer + len); pp++)
-    {
-      sprintf ((SANE_String) buffer_byte, " %02x", *pp);
-      strcat ((SANE_String) buffer_byte_list, (SANE_String) buffer_byte);
-      if (((pp - buffer) % 0x10 == 0x0f) || (pp >= (buffer + len - 1)))
-	{
-	  DBG (5, "buffer: %s\n", buffer_byte_list);
-	  buffer_byte_list[0] = '\0';
-	}
-    }
-}
